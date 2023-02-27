5ae7c220bd230a4b0e62832ed4e30fadad29d45b
vkg <vkg@gmail.com>
2023-02-27 11:10:19 +0530

    Pom file changed

--- src/main/java/com/example/webapp/HelloServlet.java
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -8,29 +8,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 public class HelloServlet extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger(HelloServlet.class);
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-
-response.setContentType("text/html;charset=UTF-8");
-
-try {
-    PrintWriter out = response.getWriter();
-    
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>Hello Servlet</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>Hello, World!</h1>");
-    out.println("</body>");
-    out.println("</html>");
-} catch (IOException e) {
-    // Handle the exception here
-    log.error("Error writing response", e);
-    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-}
-}
-
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try {
+            PrintWriter out = response.getWriter();
+            
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello, World!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException e) {
+            log.error("IOException occurred: " + e.getMessage());
+        }
+    }
 }
