1232c34c1de040ae914ea9fb9824bbb7a2d645e4
vkg <vkg@gmail.com>
2023-02-27 10:35:16 +0530

    code changed

--- src/main/java/com/example/webapp/HelloServlet.java
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -10,11 +10,25 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Hello, World!</h1>");
-        out.println("</body></html>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try {
+            PrintWriter out = response.getWriter();
+            
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello, World!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException e) {
+            // Handle the exception here
+            e.printStackTrace();
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Web App</title>
+</head>
 <body>
     <h1>Welcome to my Web App</h1>
     <p>Click the button below to see a greeting from the server</p>
