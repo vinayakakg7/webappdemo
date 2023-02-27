e9d0b07341e1d64b380803df0207f9528d9f795c
vkg <vkg@gmail.com>
2023-02-27 11:02:40 +0530

    code changed logproperties

--- src/main/java/com/example/webapp/HelloServlet.java
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -11,29 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        response.setContentType("text/html;charset=UTF-8");
-        
-        try {
-            PrintWriter out = response.getWriter();
-            
-            out.println("<!DOCTYPE html>"); // Add the DOCTYPE declaration
-            out.println("<html lang=\"en\">"); // Add the lang attribute
-            out.println("<head>");
-            out.println("<title>Hello Servlet</title>"); // Add the title element
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Welcome to my Web App</h1>");
-            out.println("<p>Click the button below to see a greeting from the server</p>");
-            out.println("<form action=\"hello\" method=\"get\">");
-            out.println("<button type=\"submit\">Say Hello</button>");
-            out.println("</form>");
-            out.println("</body>");
-            out.println("</html>");
-        } catch (IOException e) {
-            // Handle the exception here
-            e.printStackTrace();
-        }
-    }
+    throws ServletException, IOException {
+
+response.setContentType("text/html;charset=UTF-8");
+
+try {
+    PrintWriter out = response.getWriter();
+    
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Hello Servlet</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Hello, World!</h1>");
+    out.println("</body>");
+    out.println("</html>");
+} catch (IOException e) {
+    // Handle the exception here
+    log.error("Error writing response", e);
+    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+}
+}
+
 }
